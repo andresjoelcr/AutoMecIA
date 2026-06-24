46376a50e6bfd3b06ebe6f54cf03737561003fa6
andresjoelcr <andrescabrerarjoel@gmail.com>
2026-06-24 00:23:26 -0500

    Configurar STATIC_ROOT para Render

--- chatmecanica/settings.py
+++ chatmecanica/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # OpenAI API Key Configuration
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
